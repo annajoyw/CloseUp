597c9a24c80844c3b9604dd83f94697911aeb9ee
annajoyw <arealjoyw@gmail.com>
2021-04-06 21:49:23 -0400

    reply and resource classes added

---
+++ CloseUp.Data/HelpResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseUp.Data
+{
+    public class HelpResource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string ResourceInfo { get; set; }
+    }
+}
---
+++ CloseUp.Data/Reply.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseUp.Data
+{
+    class Reply
+    {
+        [Key]
+        public int ReplyId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+        [ForeignKey("JournalEntry")]
+        public int JournalEntryId { get; set; }
+        public virtual JournalEntry JournalEntry { get; set; }
+
+    }
+}
