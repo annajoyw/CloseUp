e9eccb1d837be16f27fbc88e9b9bea8fd07692db
annajoyw <arealjoyw@gmail.com>
2021-04-13 18:19:08 -0400

    added prompt files

---
+++ CloseUp/Controllers/PromptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloseUp.Controllers
+{
+    public class PromptController : Controller
+    {
+        // GET: Prompt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
